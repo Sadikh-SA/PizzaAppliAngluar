ec8a737a3dc7fc4fb807e549d3d930f17c67887b
ababacar gueye <sadikhabou@gmail.com>
2021-10-18 14:32:16 +0200

    Génération des composant ajout, liste et détail de pizza et le header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PizzaComponent } from './pizza/pizza.component';
+import { PizzaDetailComponent } from './pizza-detail/pizza-detail.component';
+import { PizzaListComponent } from './pizza-list/pizza-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaComponent,
+    PizzaDetailComponent,
+    PizzaListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
